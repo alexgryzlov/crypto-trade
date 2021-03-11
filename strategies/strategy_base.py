561c8cf5fa25d11f90956b6eed0247a85bd739ac
Marchello00 <nagovitsin.ms@phystech.edu>
2021-03-11 23:03:12 +0300

    CTS-29: strategies typed and refactored

--- strategies/strategy_base.py
+++ strategies/strategy_base.py
@@ -1,15 +1,15 @@
-from trading import Trend, TrendType
+from trading import Trend, TrendType, Order
 
 
 class StrategyBase:
-    def handle_new_trend_signal(self, trend: Trend):
+    def handle_new_trend_signal(self, trend: Trend) -> None:
         pass
 
-    def handle_extremum_signal(self, trend: TrendType):
+    def handle_extremum_signal(self, trend: TrendType) -> None:
         pass
 
-    def handle_moving_average_signal(self, trend: TrendType):
+    def handle_moving_average_signal(self, trend: TrendType) -> None:
         pass
 
-    def handle_filled_order_signal(self, order):
+    def handle_filled_order_signal(self, order: Order) -> None:
         pass
