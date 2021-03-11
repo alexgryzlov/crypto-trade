3e520b1667e5b6de1b609873d0b84edbd643315e
Marchello00 <nagovitsin.ms@phystech.edu>
2021-03-11 23:34:42 +0300

    CTS-29: trading_signal_detectors typed and refactored

--- trading_signal_detectors/extremum/extremum_signal_detector.py
+++ trading_signal_detectors/extremum/extremum_signal_detector.py
@@ -1,40 +1,48 @@
-from trading_signal_detectors.trading_signal_detector import TradingSignalDetector
+from trading_signal_detectors.trading_signal_detector import \
+    TradingSignalDetector
 
 from trading import Signal, TrendType
+import trading_system.trading_system as ts
 
 from logger.logger import Logger
+
+import typing as tp
 
 PRICE_SHIFT = 0.005
 
 
 class ExtremumSignalDetector(TradingSignalDetector):
-    def __init__(self, trading_system, extremum_count):
+    def __init__(self, trading_system: ts.TradingSystem, extremum_count: int):
         self.logger = Logger('ExtremumSignalDetector')
         self.ts = trading_system
         self.extremum_count = extremum_count
-        self.local_maximums = []
-        self.local_minimums = []
+        self.local_maximums: tp.List[float] = []
+        self.local_minimums: tp.List[float] = []
         self.last_candle_timestamp = -1
 
-    def get_trading_signals(self):
+    def get_trading_signals(self) -> tp.List[Signal]:
         self.__update()
 
         if len(self.local_minimums) >= self.extremum_count and \
                 len(self.local_maximums) >= self.extremum_count:
 
-            if self.__is_increasing(self.local_maximums[-self.extremum_count:]) and \
-                    self.__is_increasing(self.local_minimums[-self.extremum_count:]):
+            if self.__is_increasing(
+                    self.local_maximums[-self.extremum_count:]) and \
+                    self.__is_increasing(
+                        self.local_minimums[-self.extremum_count:]):
                 self.logger.info("Extremum uptrend detected")
                 return [Signal("extremum", TrendType.UPTREND)]
 
-            if self.__is_decreasing(self.local_maximums[-self.extremum_count:]) and \
-                    self.__is_decreasing(self.local_minimums[-self.extremum_count:]):
+            if self.__is_decreasing(
+                    self.local_maximums[-self.extremum_count:]) and \
+                    self.__is_decreasing(
+                        self.local_minimums[-self.extremum_count:]):
                 self.logger.info("Extremum downtrend detected")
                 return [Signal("extremum", TrendType.DOWNTREND)]
 
         return []
 
-    def __update(self):
+    def __update(self) -> None:
         candles = self.ts.get_last_n_candles(3)
         if len(candles) < 3:
             return
@@ -49,17 +57,21 @@ class ExtremumSignalDetector(TradingSignalDetector):
         if self.__is_local_minimum(*[c.get_lower_price() for c in candles]):
             self.local_minimums.append(candles[-2].get_lower_price())
 
-    def __is_increasing(self, arr):
+    def __is_increasing(self, arr: tp.List[float]) -> bool:
         for i in range(len(arr) - 1):
             if arr[i] * (1 + PRICE_SHIFT) > arr[i + 1]:
                 return False
         return True
 
-    def __is_decreasing(self, arr):
+    def __is_decreasing(self, arr: tp.List[float]) -> bool:
         return self.__is_increasing(list(reversed(arr)))
 
-    def __is_local_maximum(self, left_candle, middle_candle, right_candle):
-        return max(left_candle, right_candle) * (1 + PRICE_SHIFT) < middle_candle
+    def __is_local_maximum(self, left_candle: float, middle_candle: float,
+                           right_candle: float) -> bool:
+        return max(left_candle, right_candle) * (
+                1 + PRICE_SHIFT) < middle_candle
 
-    def __is_local_minimum(self, left_candle, middle_candle, right_candle):
-        return min(left_candle, right_candle) * (1 - PRICE_SHIFT) > middle_candle
+    def __is_local_minimum(self, left_candle: float, middle_candle: float,
+                           right_candle: float) -> bool:
+        return min(left_candle, right_candle) * (
+                1 - PRICE_SHIFT) > middle_candle
