e5f42d2589bc6dee3ff25fab8ec60838814435d0
Marchello00 <nagovitsin.ms@phystech.edu>
2021-03-12 00:27:37 +0300

    CTS-29: trading_system typed and refactored

--- trading_system/indicators/relative_strength_index_handler.py
+++ trading_system/indicators/relative_strength_index_handler.py
@@ -1,26 +1,30 @@
 from math import isclose
 
-from trading_system.indicators.exp_moving_average_handler import ExpMovingAverageHandler
+from trading_system.indicators.exp_moving_average_handler import \
+    ExpMovingAverageHandler
 from trading_system.trading_system_handler import TradingSystemHandler
 from trading_interface.trading_interface import TradingInterface
+
+import typing as tp
 
 
 class RelativeStrengthIndexHandler(TradingSystemHandler):
     """ Relative Strength Index (RSI) """
-    def __init__(self, trading_interface: TradingInterface, window_size):
+
+    def __init__(self, trading_interface: TradingInterface, window_size: int):
         super().__init__(trading_interface)
         self.ti = trading_interface
 
         self.window_size = window_size
         self.alpha = 1 / window_size
 
-        self.relative_strength = []
-        self.values = []
+        self.relative_strength: tp.List[float] = []
+        self.values: tp.List[float] = []
 
-    def get_name(self):
+    def get_name(self) -> str:
         return f'{type(self).__name__}{self.window_size}'
 
-    def update(self):
+    def update(self) -> None:
         if not super().received_new_candle():
             return
 
@@ -33,20 +37,25 @@ class RelativeStrengthIndexHandler(TradingSystemHandler):
         self.relative_strength.append(rs)
         self.values.append(rsi)
 
-    def get_last_n_values(self, n):
+    def get_last_n_values(self, n: int) -> tp.List[float]:
         return self.values[-n:]
 
     @staticmethod
-    def calculate_from(deltas, alpha=None) -> [float, float]:
+    def calculate_from(deltas: tp.List[float],
+                       alpha: tp.Union[None, float] = None) \
+            -> tp.Tuple[float, float]:
         """ Returns relative strength value and relative strength index. """
         if alpha is None:
             alpha = 1 / len(deltas)
 
-        average_gain = ExpMovingAverageHandler.calculate_from(list(map(lambda x: max(x, 0), deltas)), alpha)
-        average_loss = ExpMovingAverageHandler.calculate_from(list(map(lambda x: max(-x, 0), deltas)), alpha)
+        average_gain = ExpMovingAverageHandler.calculate_from(
+            list(map(lambda x: max(x, 0), deltas)), alpha)
+        average_loss = ExpMovingAverageHandler.calculate_from(
+            list(map(lambda x: max(-x, 0), deltas)), alpha)
 
         if average_loss == 0:  # Avoid RuntimeWarning with zero division
-            relative_strength = 1 if isclose(average_gain, 0, abs_tol=1e-7) else float('inf')
+            relative_strength = 1 if isclose(average_gain, 0,
+                                             abs_tol=1e-7) else float('inf')
         else:
             relative_strength = average_gain / average_loss
         relative_strength_index = 100 - 100 / (1 + relative_strength)
--- trading_system/orders_handler.py
+++ trading_system/orders_handler.py
@@ -6,29 +6,33 @@ from trading import Order
 from logger.log_events import FilledOrderEvent
 from logger.logger import Logger
 
+import typing as tp
+
 
 class OrdersHandler(TradingSystemHandler):
     def __init__(self, trading_interface: TradingInterface):
         super().__init__(trading_interface)
         self.ti = trading_interface
         self.logger = Logger("OrdersHandler")
-        self.active_orders = []
-        self.new_filled_orders = []
+        self.active_orders: tp.List[Order] = []
+        self.new_filled_orders: tp.List[Order] = []
 
-    def update(self):
-        filled_orders = [order for order in self.active_orders if self.ti.order_is_filled(order)]
+    def update(self) -> None:
+        filled_orders = [order for order in self.active_orders if
+                         self.ti.order_is_filled(order)]
         for order in filled_orders:
             self.logger.trading(FilledOrderEvent(order.order_id))
         self.new_filled_orders += filled_orders
-        self.active_orders = [order for order in self.active_orders if not self.ti.order_is_filled(order)]
+        self.active_orders = [order for order in self.active_orders if
+                              not self.ti.order_is_filled(order)]
 
-    def get_active_orders(self):
+    def get_active_orders(self) -> tp.List[Order]:
         return self.active_orders
 
-    def get_new_filled_orders(self):
+    def get_new_filled_orders(self) -> tp.List[Order]:
         orders = self.new_filled_orders
         self.new_filled_orders = []
         return orders
 
-    def add_new_order(self, order: Order):
+    def add_new_order(self, order: Order) -> None:
         self.active_orders.append(order)
