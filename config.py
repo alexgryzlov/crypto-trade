4cac88322e5c5e98a85fcbec80cc0e6184111221
Anton Kulikov <kulikov.anton@phystech.edu>
2021-03-19 20:10:12 +0300

    [CTS-32] fix types

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 # --- Market data ---
 
-CANDLES_PER_REQUEST = int(os.getenv('CANDLES_PER_REQUEST'))
+CANDLES_PER_REQUEST = int(os.getenv('CANDLES_PER_REQUEST') or '')
 MARKET_DATA_HOST = os.getenv('MARKET_DATA_HOST')
 MATCHER_HOST = os.getenv('MATCHER_HOST')
 
 # --- Simulator ---
 
-PRICE_SHIFT = float(os.getenv('PRICE_SHIFT'))
+PRICE_SHIFT = float(os.getenv('PRICE_SHIFT') or '')
