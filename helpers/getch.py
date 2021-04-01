e3f0ed22c4a57299ee6f0b641db1a96c5d964c22
Anton Kulikov <kulikov.anton@phystech.edu>
2021-04-01 17:16:17 +0300

    [CTS-80] add windows support for getch

--- helpers/getch.py
+++ helpers/getch.py
@@ -1,14 +1,33 @@
-import sys
-import tty
-import termios
+import os
 
 
-def getch() -> str:
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setcbreak(fd)
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+class _Getch:
+    def __init__(self) -> None:
+        if os.name == 'nt':
+            self.getch = _Getch.__getch_windows
+        else:
+            self.getch = _Getch.__getch_unix
+
+    @staticmethod
+    def __getch_unix() -> str:
+        import sys
+        import tty
+        import termios
+
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setcbreak(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    @staticmethod
+    def __getch_windows() -> str:
+        import msvcrt
+
+        return msvcrt.getch()  # type: ignore
+
+
+getch = _Getch().getch
