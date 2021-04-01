d18203f1ffb8a874ed2cce124e2bcd779cc2bdf5
Anton Kulikov <kulikov.anton@phystech.edu>
2021-04-01 16:04:02 +0300

    [CTS-80] Redesign orderbook download loop

--- scripts/retrieving_orderbook.py
+++ scripts/retrieving_orderbook.py
@@ -1,22 +1,28 @@
 '''
-При запуске скрипт создает в текущей директории папку с названием "orderbook_<гггг-мм-дд>",
-в которой каждую минуту сохраняет csv файлы с данными из стакана для валютной пары WAVES-USDN.
-Файлы имеют названия вида "<чч-мм-сс>.csv". Если в течении работы скрипта наступает следующий день
+При запуске скрипт создает в текущей директории папку с названием
+"orderbook_<гггг-мм-дд>", в которой каждую минуту сохраняет csv файлы с данными
+из стакана для валютной пары WAVES-USDN. Файлы имеют названия вида
+"<чч-мм-сс>.csv". Если в течении работы скрипта наступает следующий день
 создаётся новая папка "orderbook_<гггг-мм-дд>" в текущей директории.
 '''
 
-import time
 import csv
-from datetime import datetime, timedelta
+import select
+import sys
+from datetime import datetime
 from pathlib import Path
-from trading.asset import Asset
-from trading.asset import AssetPair
+
+from base.config_parser import ConfigParser
+from helpers.timeout_decorator import timeout
 from market_data_api.market_data_downloader import MarketDataDownloader
+from trading.asset import Asset, AssetPair
 
 ASSET_1 = Asset('WAVES')
 ASSET_2 = Asset('USDN')
-TIME_DELTA = timedelta(minutes=1)
+TIME_DELTA = 60  # seconds
 
+base_config = ConfigParser.load_config(Path('configs/base.json'))
+MarketDataDownloader.init(base_config)
 path = Path.cwd()
 today = datetime.today().strftime('%Y-%m-%d')
 start_time = datetime.today()
@@ -36,13 +42,18 @@ def update_folder() -> None:
     folder.mkdir(exist_ok=True)
 
 
-update_folder()
+def load_orderbook() -> None:
+    global today
+    global start_time
+    global folder
+    global counter
 
-while True:
+    print('Loading orderbook... ', end='')
     if today != datetime.today().strftime('%Y-%m-%d'):
         update_folder()
     res = MarketDataDownloader.get_orderbook(AssetPair(ASSET_1, ASSET_2))
-    readable_time = datetime.fromtimestamp(res['timestamp'] / 1000).strftime("%H-%M-%S")
+    readable_time = \
+        datetime.fromtimestamp(res['timestamp'] / 1000).strftime("%H-%M-%S")
     with open(folder / (readable_time + '-bid.csv'), 'w') as f_bid, \
          open(folder / (readable_time + '-ask.csv'), 'w') as f_ask:
         fnames = ['price', 'amount']
@@ -51,5 +62,27 @@ while True:
             writer.writeheader()
             for row in res[price_type]:
                 writer.writerow(dict(zip(fnames, row)))
-    time.sleep(((start_time - datetime.today()) + counter * TIME_DELTA).total_seconds())
     counter += 1
+    print('Done.')
+
+
+@timeout(TIME_DELTA)
+def read_input() -> None:
+    while True:
+        x = sys.stdin.read(1) \
+            if select.select([sys.stdin], [], [], 0)[0] != [] else None
+        if x == 'q':
+            print('Stopping execution...')
+            break
+
+
+if __name__ == '__main__':
+    print('Press `q` to stop downloading orderbook.')
+    update_folder()
+    load_orderbook()
+    while True:
+        try:
+            read_input()
+            break
+        except TimeoutError:
+            load_orderbook()
