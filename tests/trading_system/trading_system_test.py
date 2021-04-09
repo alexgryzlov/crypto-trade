3ab02b852c901cf40a1327ad173bc092eb436d57
Alexander Gryzlov <gryzlov.ad@phystech.edu>
2021-04-10 01:54:21 +0300

    [CTS-58] add more tests

--- tests/trading_system/trading_system_test.py
+++ tests/trading_system/trading_system_test.py
@@ -8,8 +8,15 @@ from trading_system.trading_system import Handlers, TradingSystem
 from trading import Order, AssetPair, Asset, Direction
 
 one_values = [1] * 10
+real_values = [10.2717, 10.295, 10.330, 10.332, 10.326, 10.303, 10.355, 10.341,
+               10.277, 10.270, 10.256, 10.230, 10.270,
+               10.247, 10.251, 10.263, 10.223, 10.220, 10.220, 10.257, 10.318,
+               10.291, 10.297, 10.286, 10.289]
+
 ones_ti = TradingInterfaceMock.from_price_values(one_values)
+real_ti = TradingInterfaceMock.from_price_values(real_values)
 ones_ti.order_is_filled = MagicMock(return_value=True)
+real_ti.order_is_filled = MagicMock(return_value=True)
 
 
 @pytest.fixture
@@ -68,3 +75,48 @@ def test_trading_system_filled_signal(ti: TradingInterfaceMock, ts, empty_logger
     assert len(ts.get_trading_signals()) == 0
     assert len(signals) >= 2
     assert any(x.name == 'filled_order' for x in signals)
+
+
+@pytest.mark.parametrize('ts', [ones_ti], indirect=True)
+@pytest.mark.parametrize('ti', [ones_ti])
+def test_get_total_balance(ti: TradingInterfaceMock, ts, empty_logger_mock):
+    ti.update()
+
+
+
+@pytest.mark.parametrize('ts', [ones_ti], indirect=True)
+@pytest.mark.parametrize('ti', [ones_ti])
+def test_trading_system_stop_trading(ti: TradingInterfaceMock, ts, empty_logger_mock):
+    ti.update()
+    init_balance = 30
+    ts.wallet[Asset("USDN")] = init_balance
+    ts.wallet[Asset("WAVES")] = 0
+    ts.buy(AssetPair(Asset("USDN"), Asset("WAVES")), 1, 10)
+    ts.buy(AssetPair(Asset("USDN"), Asset("WAVES")), 1, 10)
+    assert ts.wallet[Asset("USDN")] == init_balance - 20
+    assert len(ts.get_active_orders()) == 2
+    ts.stop_trading()
+    ts.update()
+    assert len(ts.get_active_orders()) == 0
+    assert ts.wallet[Asset("USDN")] == init_balance
+
+
+@pytest.mark.parametrize('ts', [real_ti], indirect=True)
+@pytest.mark.parametrize('ti', [real_ti])
+def test_trading_system_get_total_balance(ti: TradingInterfaceMock, ts, empty_logger_mock):
+    ti.update()
+    init_balance_usdn = 100
+    init_balance_waves = 10
+    ts.wallet[Asset("USDN")] = init_balance_usdn
+    ts.wallet[Asset("WAVES")] = init_balance_waves
+    assert pytest.approx(ts.get_total_balance(), 1e-6) ==\
+           (init_balance_usdn + init_balance_waves * ts.get_price_by_direction(Direction.SELL))
+    ts.sell(AssetPair(Asset("USDN"), Asset("WAVES")), 10, 10)
+    ts.update()
+    assert pytest.approx(ts.get_total_balance(), 1e-6) == init_balance_usdn + 10 * 10
+    ts.buy(AssetPair(Asset("USDN"), Asset("WAVES")), 20, 10)
+    ts.update()
+    ti.update()
+    ti.update()
+    assert pytest.approx(ts.get_total_balance(), 1e-6) ==\
+           (20 * ts.get_price_by_direction(Direction.SELL))
