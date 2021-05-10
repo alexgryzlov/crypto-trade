7e9683b954bcccb47d08fc79e1d345cb1ede502d
Anton Kulikov <kulikov.anton@phystech.edu>
2021-05-10 04:06:45 +0300

    [CTS-144] add `timeout` parameter

--- strategies/adaptable_grid_strategy/adaptable_grid_strategy.py
+++ strategies/adaptable_grid_strategy/adaptable_grid_strategy.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+import trading_system.trading_system as ts
 from helpers.typing.common_types import Config
 from strategies.grid_strategy.grid_strategy import GridStrategy
 from trading import Order
@@ -11,6 +12,10 @@ class AdaptableGridStrategy(GridStrategy):  # type: ignore
         self.threshold = config['threshold']
         self.window = config['window']
         self.coef = config['coef']
+        self.timeout = config['candles_timeout'] * config['candles_lifetime']
+        self.timeout_only = config['timeout_only']
+        self.handle_filled_orders = config['handle_filled_orders']
+        self.ticks = 0
 
     def calculate_base_price(self) -> float:
         return self.__get_last_n_mid_prices().mean()
@@ -19,22 +24,31 @@ class AdaptableGridStrategy(GridStrategy):  # type: ignore
         return (self.coef * self.__get_last_n_mid_prices().std() /
                 self.base_level)
 
+    def init_trading(self, trading_system: ts.TradingSystem) -> None:
+        self.ts = trading_system
+        # not placing orders at the start
+        # waiting for enough candles to calculate mean and variance
+
     def update(self) -> None:
         new_interval = self.calculate_interval()
         new_base = self.calculate_base_price()
-        if (self.__rel_diff(new_interval, self.interval) >  # type: ignore
-                self.threshold or
+        condition = self.ticks > self.timeout or (
+            not self.timeout_only and (
                 self.__rel_diff(new_base, self.base_price) >  # type: ignore
-                self.threshold):
+                self.threshold))
+        if condition:
             self.logger.info(f'Updating grid: new base price = {new_base}, '
                              f'new interval = {new_interval}')
             self.base_price = new_base
             self.interval = new_interval
             self.ts.cancel_all()
             self.place_orders()
+            self.ticks = 0
+        self.ticks += 1
 
     def handle_filled_order_signal(self, order: Order) -> None:
-        pass
+        if self.handle_filled_orders:
+            super().handle_filled_order_signal(order)
 
     def __get_last_n_mid_prices(self) -> np.array:
         return np.array(list(
